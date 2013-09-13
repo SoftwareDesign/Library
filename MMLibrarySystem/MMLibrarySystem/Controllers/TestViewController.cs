bdd14e5f64d831f41a2285f722c186acb7202519
xiaoshenmanbu <106287961@qq.com>
2013-09-13 08:57:01 +0800

    The Second Commit

---
+++ MMLibrarySystem/MMLibrarySystem/Controllers/TestViewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MMLibrarySystem.Controllers
+{
+    public class TestViewController : Controller
+    {
+        //
+        // GET: /TestView/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TestView2()
+        {
+            return View();
+        }
+        public ActionResult TestView3()
+        {
+            return View();
+        }
+    }
+}
