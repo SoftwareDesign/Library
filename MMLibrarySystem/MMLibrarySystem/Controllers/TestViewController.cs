b8ff481d9ec82bfef0383ee2c8613885794b5ee4
xiaoshenmanbu <106287961@qq.com>
2013-09-18 10:17:27 +0800

    We Can Get the Information now!

--- MMLibrarySystem/MMLibrarySystem/Controllers/TestViewController.cs
+++ MMLibrarySystem/MMLibrarySystem/Controllers/TestViewController.cs
@@ -14,23 +14,16 @@ namespace MMLibrarySystem.Controllers
 
         public ActionResult Index()
         {
-            //using (var dbContext = new BookLibraryContext())
-            //{
-            //    var bookinfo = new BookInfo()
-            //    {
-            //        Title = "Test"
-            //    };
-            //    var book = new Book()
-            //    {
-            //        BookNumber = "22222",
-            //        BookInfo = bookinfo
-            //    };
+            List<Book> books;
+            using (var dbContext = new BookLibraryContext())
+            {
+                books = dbContext.Books.Include("BookInfo").ToList();
 
-            //    dbContext.Books.Add(book);
-            //    dbContext.SaveChanges();
-            //}
-            return View();
+            }
+
+            return View(books);
         }
+
         public ActionResult TestView2()
         {
             return View();
--- MMLibrarySystem/MMLibrarySystem/Models/BookLibraryContext.cs
+++ MMLibrarySystem/MMLibrarySystem/Models/BookLibraryContext.cs
@@ -7,11 +7,17 @@ using System.Data.Entity;
 namespace MMLibrarySystem.Models
 {
     public class BookLibraryContext : DbContext
-    {
+    {       
         public DbSet<Book> Books
         { get; set; }
 
         public DbSet<BookInfo> BookInfos
         { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.Configuration.LazyLoadingEnabled = false;
+            base.OnModelCreating(modelBuilder);            
+        }
     }
 }
