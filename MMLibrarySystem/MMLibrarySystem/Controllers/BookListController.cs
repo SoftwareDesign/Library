799aa81d08bfaced32b04d279597b5eda9006946
xiaoshenmanbu <106287961@qq.com>
2013-09-24 09:43:06 +0800

    Add search logic

---
+++ MMLibrarySystem/MMLibrarySystem/Controllers/BookListController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MMLibrarySystem.Models;
+
+namespace MMLibrarySystem.Controllers
+{
+    public class BookListController : Controller
+    {      
+        public ActionResult Index(string searchTerm = null)
+        {
+            List<Book> books = new List<Book>();
+            using (var dbContext = new BookLibraryContext())
+            {
+                var tempBooks = dbContext.Books.Include("BookInfo");
+
+                if (string.IsNullOrEmpty(searchTerm))
+                {
+                    books.AddRange(tempBooks.ToList());
+                }
+                else
+                {
+                    books.AddRange(tempBooks.Where(b => b.BookInfo.Title.Contains(searchTerm) || b.BookInfo.Description.Contains(searchTerm)));
+                }
+            }
+
+            if (Request.IsAjaxRequest())
+            {
+                PartialView("_BookList", books);
+            }
+
+            return View(books);
+        }
+    }
+}
