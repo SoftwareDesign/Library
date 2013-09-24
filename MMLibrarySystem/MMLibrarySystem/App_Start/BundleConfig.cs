ee21558cae3d1fabb90513b8a640acfae1cb168f
xiaoshenmanbu <106287961@qq.com>
2013-09-24 09:54:13 +0800

    ajax search

--- MMLibrarySystem/MMLibrarySystem/App_Start/BundleConfig.cs
+++ MMLibrarySystem/MMLibrarySystem/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace MMLibrarySystem
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
