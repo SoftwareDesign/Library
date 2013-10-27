e6aeb3494d85a7723aeac73e5dd116594d27a1b9
daseny <daseny@gmail.com>
2013-10-27 23:48:52 +0800

    Added index view for the role admin (data still need be udpated).

---
+++ MMLibrarySystem/MMLibrarySystem/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MMLibrarySystem.Models;
+
+namespace MMLibrarySystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            var infos = new List<BorrowInfo>();
+            using (var dbContext = new BookLibraryContext())
+            {
+                var allInfos = dbContext.BorrowInfos;
+                infos.AddRange(allInfos);
+            }
+
+            return View(infos);
+        }
+    }
+}
