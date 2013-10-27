f0766c833bf4eea618542245930ff64d401daea5
xiaoshenmanbu <106287961@qq.com>
2013-10-27 19:54:02 +0800

    Add some files

---
+++ MMLibrarySystem/MMLibrarySystem/Dto/PageInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace MMLibrarySystem.Dto
+{
+    public class PageInfo
+    {
+        public PageInfo()
+        {
+            XmlDocument doc=new XmlDocument();
+            doc.Load("");
+        }
+        private int _totalBookNumber = 0;
+        public int PageSize { get; private set; }
+
+        public int TotalBookNumber
+        {
+            get { return _totalBookNumber; }
+            set
+            { 
+                _totalBookNumber = value;
+                PageCount = _totalBookNumber/PageSize + 1;
+            }
+        }
+
+        public int PageCount { get; private set; }
+    }
+}
---
+++ MMLibrarySystem/MMLibrarySystem/Scripts/PagerList.js
@@ -0,0 +1,19 @@
+﻿//$(function () {
+    var getPage = function () {
+//        var $a = $(this);
+//        var options = {
+//            url: $a.attr("href"),
+//            type: get
+//        };
+//        $.ajax(options).done(function (data) {
+//            var target = $a.parents("div.pageList").attr("mytarget");
+//            $(target).replaceWith(data);
+//        });
+
+//        return false;
+    };
+
+
+   // $(".main-content").on("click", ".pageList a", alert("aaa"));
+//});
+
