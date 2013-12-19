9d6a8675eb26dcec5625542d9edbf6b67fb60d0b
daseny <daseny@gmail.com>
2013-12-20 00:08:56 +0800

    Created installer for the schedule service #59.

---
+++ MMLibrarySystem/SharedAssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Defined shared assembly info at here
+[assembly: AssemblyCompany("SigSD")]
+[assembly: AssemblyProduct("Book Library")]
+[assembly: AssemblyCopyright("Copyleft © SigSD 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.2013.12091")]
+[assembly: AssemblyFileVersion("0.1.2013.12091")]
